5e71bb49ffb3cc57c37977ed85e2475ded6fb598
viitor14 <manoelvitor550@gmail.com>
2025-04-24 23:12:24 -0300

    mudanças na consulta do beneficiario

--- src/routes/beneficiarioRoutes.js
+++ src/routes/beneficiarioRoutes.js
@@ -4,7 +4,7 @@ import beneficiarioController from '../controllers/BeneficiarioController';
 const router = new Router();
 
 router.post('/', beneficiarioController.store);
-router.get('/', beneficiarioController.Active);
+router.get('/ativo', beneficiarioController.Active);
 router.get('/all', beneficiarioController.All);
 router.put('/:id', beneficiarioController.update);
 
